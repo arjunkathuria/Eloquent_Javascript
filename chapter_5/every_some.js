355e7ea7e439a4baf1cb9a7e02a09abe0b73370d
Arjun Kathuria <arjun.kathuria8@gmail.com>
2015-09-07 02:40:57 +0530

    Chatper 5 complete : - solves the every and some problem

---
+++ chapter_5/every_some.js
@@ -0,0 +1,19 @@
+function every(array,f) {
+	return array.map(function(element){return f(element)}).reduce(function(a,b){return a && b});
+}
+
+function some(array,f){
+	for (var i = 0; i < array.length; i++) {
+		if(f(array[i])){return true;}
+		
+	}
+	return false;
+}
+
+console.log(every([NaN, NaN, NaN], isNaN));
+// → true
+console.log(every([NaN, NaN, 4], isNaN));
+// → false
+console.log(some([NaN, 3, 4], isNaN));
+// → true
+console.log(some([2, 3, 4], isNaN));
