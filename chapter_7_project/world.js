55e3d941747dea84a688f5728c674a79b7bc7aa9
Arjun Kathuria <arjun.kathuria8@gmail.com>
2016-02-08 00:29:48 +0530

    adds the View constructor with prototypes, adds checkDestination, turn and letAct methods in World prototypes

--- chapter_7_project/world.js
+++ chapter_7_project/world.js
@@ -18,7 +18,7 @@ function Vector(x,y){
 }
 Vector.prototype.plus = function(other){
   return new Vector(this.x+other.x, this.y+other.y) ;
-}
+};
 
 // grid to represented as an array of size length x width
 // where an element (x,y) is found at (x,y*width)
@@ -61,9 +61,9 @@ var directionNames = "n ne e se s sw w nw".split(" ");
 function BouncingCritter(){
 
   this.direction = randomElement(directionNames);
-};
+}
 
-BouncingCritter.prototype.act = function(view){i
+BouncingCritter.prototype.act = function(view){
   if(view.look(this.direction) != " ")
     this.direction = view.find(" ") || "s";
   return {type: "move",direction: this.direction};
@@ -111,8 +111,81 @@ World.prototype.toString = function() {
   return output;
 };
 
-function Wall() {};
+function Wall() {}
 
-var world = new World(plan,{'#':Wall,'o':BouncingCritter});
+var world = new World(plan,{"#":Wall, "o":BouncingCritter});
+
 
 console.log(world.toString());
+
+Grid.prototype.forEach = function (f,context) {
+  for (var y = 0; y < this.width; y++) {
+    for (var x = 0; x < this.height; x++) {
+      var value =this.space[x + y * this.width]  ;
+      if (value != null) {
+        f.call(context, value, new Vector(x,y));
+      }
+    }
+  }
+};
+
+World.prototype.turn = function () {
+  var acted = [];
+  this.grid.forEach(function (critter, vector) {
+    if (critter.act && acted.indexOf(critter) == -1) {
+      acted.push(critter);
+      this.letAct(critter, vector);
+    }
+  },this);
+};
+
+World.prototype.letAct = function (critter, vector) {
+  var action = critter.act(new View(this, vector));
+  if (action && action.type == "move") {
+    var dest = this.checkDestination(action, vector);
+    if (dest && this.grid.get(dest)== null) {
+      this.grid.set(vector, null)  ;
+      this.grid.set(dest,critter);
+    }
+  }
+};
+
+World.prototype.checkDestination = function (action, vector) {
+  if (directions.hasOwnProperty(action.direction)) {
+
+    var dest = vector.plus(directions[action.direction]);
+    if (this.grid.isInside(dest)) {
+      return dest;
+    }
+  }
+
+};
+
+function View(world, vector) {
+  this.world = world;
+  this.vector = vector;
+}
+View.prototype.look = function (dir) {
+  var target = this.vector.plus(directions[dir]);
+  if (this.world.grid.isInside(target))
+    return charFromElement(this.world.grid.get(target));
+  else
+    return "#";
+
+};
+
+View.prototype.findAll = function (ch) {
+  var found = [];
+  for (var dir in directions) {
+    if (this.look(dir)== ch)
+      found.push(dir)  ;
+  }
+  return found;
+};
+
+View.prottype.find = function (ch) {
+  var found = this.findAll(ch);
+  if (found.length == 0)
+    return null;
+  return randomElement(found)  ;
+};
